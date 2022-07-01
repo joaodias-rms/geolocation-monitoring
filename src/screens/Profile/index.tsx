45621c2e952c4fae2e540c71c35bd784e4e7b45c
joao <jvd.ramos@hotmail.com>
2022-07-01 17:26:44 -0400

    feat: updated Profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Container, Title} from './styles';
+import {
+  Container,
+  Header,
+  Photo,
+  PhotoButton,
+  PhotoContainer,
+  UserDataContainer,
+} from './styles';
+
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {launchImageLibrary} from 'react-native-image-picker';
+
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+import {Input} from '../../components/Input';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import theme from '../../global/theme/theme';
 
 export function Profile() {
+  const [avatar, setAvatar] = useState('');
+
+  const handleSelectAvatar = async () => {
+    const imageResult = await launchImageLibrary({
+      mediaType: 'photo',
+      quality: 1,
+    });
+    if (imageResult.didCancel) {
+      return;
+    }
+    if (imageResult.assets) {
+      console.log(imageResult.assets[0].uri);
+
+      setAvatar(imageResult.assets[0].uri);
+    }
+  };
+
   return (
-    <Container>
-      <Title>Nada</Title>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <PhotoContainer>
+              <Photo source={{uri: 'https://github.com/joaodias-rms.png'}} />
+              <PhotoButton onPress={handleSelectAvatar}>
+                <Icon name="camera" size={24} color={theme.colors.text} />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
+          <UserDataContainer style={{marginBottom: useBottomTabBarHeight()}}>
+            <Input
+              iconName="credit-card"
+              placeholder="name"
+              placeholderTextColor={theme.colors.text_info}
+              autoCorrect={false}
+              autoCapitalize="sentences"
+            />
+            <Input
+              iconName="envelope"
+              placeholder="email"
+              placeholderTextColor={theme.colors.text_info}
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+          </UserDataContainer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,5 +1,44 @@
 import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  height: 100%;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 227px;
+  align-items: center;
+`;
+
+export const PhotoContainer = styled.View`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+  background-color: ${({theme}) => theme.colors.main};
+  margin-top: 48px;
+`;
+
+export const Photo = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+`;
+
+export const PhotoButton = styled(RectButton)`
+  width: 40px;
+  height: 40px;
+  background-color: ${({theme}) => theme.colors.main};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  border-radius: 8px;
+  bottom: 10px;
+  right: 10px;
+`;
+
+export const UserDataContainer = styled.View`
+  padding-top: 24px;
+`;
 
 export const Title = styled.Text``;
