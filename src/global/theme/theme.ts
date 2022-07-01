9fe983b74fde82f1cbdfbc81747da38ed6df8f9a
joao <jvd.ramos@hotmail.com>
2022-07-01 17:24:38 -0400

    style: added new style configs

--- src/global/theme/theme.ts
+++ src/global/theme/theme.ts
@@ -1,12 +1,20 @@
 export default {
   colors: {
+    main_background: '#aad',
     main: '#9000ff',
     text: '#fff',
     text_info: '#355',
+    line: '#EBEBF0',
+
+    main_light: '#FDEEDF',
+    success: '#03B252',
+
+    shape: '#E1E1E8',
+    shape_dark: '#29292E',
   },
   fonts: {
     primaryRegular: 'Gantari-Regular',
     primaryMedium: 'Gantari-Medium',
-    primaryBold: 'Gantari-Bold'
+    primaryBold: 'Gantari-Bold',
   },
 };
