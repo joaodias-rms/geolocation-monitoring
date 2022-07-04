8088510d4856e3b4a2ed0cda8858eba569d87ee2
joao <jvd.ramos@hotmail.com>
2022-07-04 17:29:00 -0400

    feat: added tracks list

--- src/screens/Tracks/index.tsx
+++ src/screens/Tracks/index.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
-import { Container } from './styles'
+import {Container, Header, HeaderTitle, TracksList} from './styles';
+
+import {Walk} from '../../database/models/walk';
+
+import {database} from '../../database';
+
+import {CardTrack} from '../../components/CardTrack';
+import {Alert} from 'react-native';
 
 export function Tracks() {
-    return (
-        <Container>
+  const [walks, setWalks] = useState<Walk[]>();
+  const [loading, setLoading] = useState(true);
 
-        </Container>
-    )
+  useEffect(() => {
+    let isMounted = true;
+    async function fetchWalks() {
+      try {
+        const walkCollection = database.get<Walk>('walks');
+        const walks = await walkCollection.query().fetch();
+
+        if (isMounted) {
+          setWalks(walks);
+        }
+      } catch (error) {
+        console.log(error);
+        Alert.alert('Attention', 'Error to get last tracked Walks');
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    }
+    fetchWalks();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Last tracked walks</HeaderTitle>
+      </Header>
+      <TracksList
+        data={walks}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <CardTrack data={item} />}
+      />
+    </Container>
+  );
 }
--- src/screens/Tracks/styles.ts
+++ src/screens/Tracks/styles.ts
@@ -1,6 +1,31 @@
-import styled from 'styled-components/native'
+import {FlatListProps, FlatList} from 'react-native';
+import styled from 'styled-components/native';
+import {Walk} from '../../database/models/walk';
+import {RFValue} from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-   flex: 1;
+  width: 100%;
+  padding: 0 24px;
+`;
 
-`
+export const Header = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 24px 32px;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: ${RFValue(28)}px;
+  font-family: ${({theme}) => theme.fonts.primaryMedium};
+  color: ${({theme}) => theme.colors.main};
+`;
+
+export const TracksList = styled(
+  FlatList as new (props: FlatListProps<Walk>) => FlatList,
+).attrs({
+  contentContainerStyle: {
+    padding: 24,
+    showVerticalScrollIndicator: false,
+  },
+})``;
