dbe27b88afdadba7628900af457af51f9ed39577
joao <jvd.ramos@hotmail.com>
2022-07-04 17:28:03 -0400

    refactor: removed unused Views

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,11 @@ import React, {useState} from 'react';
 
 import {Button} from '../../components/Button';
 import {Container, TrackedWalk} from './styles';
-import {handleStartLocation} from '../../functions/handleStartLocation';
 import theme from '../../global/theme/theme';
 import {Timer} from '../../components/Timer';
-import {View} from 'react-native';
 
 export function Home() {
-  const [running, setRunning] = useState(true);
+  const [running, setRunning] = useState(false);
   const [kilometers, setKilometers] = useState(0);
   const [meters, setMeters] = useState(0);
   function test() {
@@ -16,8 +14,11 @@ export function Home() {
   }
   return (
     <Container>
-      {running ? <Timer /> : <View />}
+      {running ? <Timer /> : null}
       <Button
+        width={150}
+        height={150}
+        iconName="map"
         title={running ? 'Stop Tracking' : 'Start Tracking'}
         color={running ? theme.colors.success : theme.colors.main}
         onPress={test}
@@ -30,7 +31,7 @@ export function Home() {
           KM
         </TrackedWalk>
       ) : (
-        <View />
+        null
       )}
     </Container>
   );
