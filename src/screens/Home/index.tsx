c9f263919d1339e8aaa5dfc4e1e26050321d9613
joao <jvd.ramos@hotmail.com>
2022-07-07 17:12:33 -0400

    feat: added tracking function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,35 +4,35 @@ import {Button} from '../../components/Button';
 import {Container, TrackedWalk} from './styles';
 import theme from '../../global/theme/theme';
 import {Timer} from '../../components/Timer';
+import {handleStartLocation} from '../../functions/handleStartLocation';
 
 export function Home() {
   const [running, setRunning] = useState(false);
   const [kilometers, setKilometers] = useState(0);
   const [meters, setMeters] = useState(0);
-  function test() {
+
+  const handleStartTracking = () => {
+    handleStartLocation();
     setRunning(!running);
-  }
+  };
+
   return (
     <Container>
-      {running ? <Timer /> : null}
+      {running ? <Timer isRunning={running} /> : null}
       <Button
         width={150}
         height={150}
         iconName="map"
         title={running ? 'Stop Tracking' : 'Start Tracking'}
         color={running ? theme.colors.success : theme.colors.main}
-        onPress={test}
+        onPress={handleStartTracking}
       />
       {running ? (
         <TrackedWalk>
-          {kilometers}
-          .
-          {meters}
+          {kilometers}.{meters}
           KM
         </TrackedWalk>
-      ) : (
-        null
-      )}
+      ) : null}
     </Container>
   );
 }
