0de9608e1be6ed67b60840340ee294a14797e549
joao <jvd.ramos@hotmail.com>
2022-07-15 17:17:34 -0400

    feat: created second step register screen

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
-import { Container } from './styles'
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  AvatarContainer,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+} from './styles';
+
+import {BackButton} from '../../../components/BackButton';
+
+import {launchImageLibrary} from 'react-native-image-picker';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import theme from '../../../global/theme/theme';
+import UserSVG from '../../../assets/user.svg';
 
 export function SignUpSecondStep() {
-    return (
-        <Container>
+  const navigation = useNavigation();
+  const [avatar, setAvatar] = useState('');
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
-        </Container>
-    )
+  const handleSelectAvatar = async () => {
+    const imageResult = await launchImageLibrary({
+      mediaType: 'photo',
+      quality: 1,
+    });
+    if (imageResult.didCancel) {
+      return;
+    }
+    if (imageResult.assets) {
+      setAvatar(imageResult.assets[0].uri!);
+    }
+  };
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <BackButton onPress={handleGoBack} color={theme.colors.main} />
+        </HeaderTop>
+        <HeaderTitle>Now let's personalize you'r profile</HeaderTitle>
+      </Header>
+      <AvatarContainer>
+        <PhotoContainer>
+          <UserSVG color={theme.colors.cancel} width={80} height={80}/>
+          <PhotoButton onPress={handleSelectAvatar}>
+            <Icon name="camera" size={24} color={theme.colors.text} />
+          </PhotoButton>
+        </PhotoContainer>
+      </AvatarContainer>
+    </Container>
+  );
 }
--- src/screens/SignUp/SignUpSecondStep/styles.ts
+++ src/screens/SignUp/SignUpSecondStep/styles.ts
@@ -1,6 +1,55 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
-   flex: 1;
+   width: 100%;
+`;
 
-`
+export const Header = styled.View`
+  height: ${getStatusBarHeight() + 32}px;
+  margin-top: 32px;
+  width: 100%;
+  align-items: flex-start;
+  padding: 0 24px;
+`;
+
+export const HeaderTop = styled.View`
+  margin-bottom: 12px;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.primaryBold};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const AvatarContainer = styled.View`
+   margin-top: 32px;
+   align-items: center;
+`;
+
+export const PhotoContainer = styled.View`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+  background-color: ${({theme}) => theme.colors.main_background};
+`;
+
+export const Photo = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+`;
+
+export const PhotoButton = styled(RectButton)`
+  width: 40px;
+  height: 40px;
+  background-color: ${({theme}) => theme.colors.main};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  border-radius: 8px;
+  bottom: 10px;
+  right: 10px;
+`;
